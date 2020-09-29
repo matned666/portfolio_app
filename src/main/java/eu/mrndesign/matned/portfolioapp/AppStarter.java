9d9599e409e3a58df2f5103cd529948c8b7d596f
matned <mat.niedbal@yahoo.com>
2020-09-29 16:08:08 +0200

    deployment problem locally done by adding repo to pom on target/

--- src/main/java/eu/mrndesign/matned/portfolioapp/AppStarter.java
+++ src/main/java/eu/mrndesign/matned/portfolioapp/AppStarter.java
@@ -3,9 +3,6 @@ package eu.mrndesign.matned.portfolioapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
 
 @SpringBootApplication
 public class AppStarter {
