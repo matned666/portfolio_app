2adeb991c84985e032f41f48ff6e5536069181f3
matned <mat.niedbal@yahoo.com>
2020-10-07 23:34:54 +0200

    try with rails root

--- src/main/java/eu/mrndesign/matned/portfolioapp/service/GraphicService.java
+++ src/main/java/eu/mrndesign/matned/portfolioapp/service/GraphicService.java
@@ -82,7 +82,7 @@ public class GraphicService {
             FtpClient ftp = new FtpClient(ftpHost, ftpPort, ftpUser, ftpPassword);
             ftp.open();
             if (!ftp.fileExistByName("http://"+ftpHost + ftpPath + "/" ,fileName)) {
-                String tmpdir = "";
+                String tmpdir;
                 if (tempDir.equals("java.io.tmpdir")){
                 tmpdir = System.getProperty("java.io.tmpdir");
                 } else {
